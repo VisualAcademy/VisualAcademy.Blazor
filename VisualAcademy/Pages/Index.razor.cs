83c20a542677f51ec97441a7f80f7e65a2417aed
WebCampKR <webcamp@outlook.kr>
2020-01-20 05:53:58 +0900

    Carousel 실행

--- VisualAcademy/Pages/Index.razor.cs
+++ VisualAcademy/Pages/Index.razor.cs
@@ -1,10 +1,22 @@
-﻿namespace VisualAcademy.Pages
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace VisualAcademy.Pages
 {
     public partial class Index
     {
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; }
+
         protected override void OnInitialized()
         {
-            
+            // Empty
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await JSRuntime.InvokeAsync<object>("RunCarousel");
         }
     }
 }
